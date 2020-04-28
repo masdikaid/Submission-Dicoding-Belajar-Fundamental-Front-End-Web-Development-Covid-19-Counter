658b7511e48af126db39e38f4a801b8b9213a587
masdika <masdika@gmail.com>
2020-04-28 17:50:50 +0700

    reconstructing

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
+import 'bootstrap'
+import 'regenerator-runtime'
 import {fetching} from "./scripts/api.js"
-import {renderContentDunia, renderItemNegara} from "./scripts/render.js"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {renderContentDunia, renderItemNegara, renderGrafik} from "./scripts/render.js"
 
 async function renderingData(){
     const ra = await fetching()
     renderContentDunia(ra)
     renderItemNegara(ra)
+    renderGrafik()
 }
 renderingData()
--- src/scripts/render.js
+++ src/scripts/render.js
@@ -1,3 +1,5 @@
+import chart from 'tui-chart'
+
 function renderContentDunia(data){
     data.forEach( d => {
         setTimeout(()=>{
@@ -21,8 +23,9 @@ function renderItemNegara(data){
     const element = document.getElementById("section")
     data.forEach( items => {
         const news = document.createElement("article")
-    news.classList.add("my-2","container","card")
+    news.classList.add("card-sm","col-md-6","col-lg-4","my-2","container")
     news.innerHTML=`
+                <div class="shadow-sm card">
                 <header class="p-2"><h6>${items.negara}</h6></header>
                 <div class="container d-flex">
                     <div class="flex-grow-1">
@@ -46,9 +49,83 @@ function renderItemNegara(data){
                         <p>${items.total}</p>
                     </div>
                 </div>
+            </div>
         `
         element.appendChild(news)
     })
 }
 
-export {renderContentDunia, renderItemNegara}
+function renderGrafik(){
+    setTimeout(()=>{
+        const element = document.querySelector("#grafik")
+        const data = {
+            categories: ["Covid-19"],
+            series: [
+                {
+                    name: 'Positif',
+                    data: getInnerDunia("terkonfirmasi")
+                },
+                {
+                    name: 'sembuh',
+                    data: getInnerDunia("sembuh")
+                },
+                {
+                    name: 'Meninggal',
+                    data: getInnerDunia("meninggal")
+                },
+            ]
+        }
+        var options = {
+            chartExportMenu: {
+                visible: false  // default is true.
+            },
+            chart: {
+                width: "400",
+                height: "400",
+                title: 'Grafik Covid-19 Dunia',
+                format: function(value, chartType, areaType, valuetype, legendName) {
+                    if (areaType === 'makingSeriesLabel') { // formatting at series area
+                        value = value + ' Jiwa';
+                    }
+        
+                    return value;
+                }
+            },
+            series: {
+                showLabel: true,
+                showLegend: true,
+                labelAlign: 'outer'
+            },
+            tooltip: {
+                suffix: '%'
+            },
+            legend: {
+                visible: false
+            }
+        };
+        var theme = {
+            series: {
+                series: {
+                    colors: [
+                        '#83b14e', '#458a3f', '#295ba0', '#2a4175', '#289399',
+                        '#289399', '#617178', '#8a9a9a', '#516f7d', '#dddddd'
+                    ]
+                },
+                label: {
+                    color: '#fff',
+                    fontFamily: 'sans-serif'
+                }
+            }
+        };
+        
+        // For apply theme
+        
+        chart.registerTheme('myTheme', theme);
+        options.theme = 'myTheme';
+        
+        chart.pieChart(element, data, options);
+    },1500)    
+
+}
+
+export {renderContentDunia, renderItemNegara, renderGrafik}
