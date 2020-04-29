42bd05ae46f7c7928ded27ec625212971d1ca4fa
masdika <masdika@gmail.com>
2020-04-30 00:00:51 +0700

    change for with foreach to looping data

--- src/scripts/searching.js
+++ src/scripts/searching.js
@@ -3,12 +3,12 @@ search.addEventListener('input',()=> {
     const input = document.getElementById("search")
     const filter = input.value.toUpperCase()
     const cont = document.querySelectorAll("negara-item>div>div>h5")
-    for (let i = 0; i < cont.length; i++) {
+    cont.forEach((v,i) => {
         const el = cont[i].innerText
         if(el.toUpperCase().indexOf(filter) > -1 ){
             document.querySelectorAll("negara-item")[i].classList.add("order-first")
         }else{
             document.querySelectorAll("negara-item")[i].classList.remove("order-first")
         }
-    }
+    })
 })
